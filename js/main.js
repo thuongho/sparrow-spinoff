a2d48ad0daccfaa65a6a17571d63c8364e4fdb87
Thuong Ho <thuong.t.ho@gmail.com>
2016-04-30 16:26:45 -0700

    load a sprite as background image

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 (function() {
   'use strict';
 
-  // let phaser decide WebGL or default to canvas if WebGL is not avail
+  // give game dimensions
+  // phaser use WebGL or default to canvas if WebGL is not avail
   // Pixi.js does the rendering of the WebGL
-  var game = new Phaser.Game(640, 360, Phaser.AUTO);
+  var game = new Phaser.Game(640, 360, Phaser.AUTO); 
 
   // game state
   var GameState = {
     // all image loaded
     preload: function() {
-
+      this.load.image('background', 'assets/images/earth_1920_1080.jpg');
     },
     create: function() {
-
+      this.background = this.game.add.sprite(0, 0, 'background') // coords top left, key
     },
     update: function() {  // running multple time per sec to get input
 

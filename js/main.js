4719259e09a7731b752523f8e508f3d6937120d0
Thuong Ho <thuong.t.ho@gmail.com>
2016-04-30 16:31:35 -0700

    add smaller background

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  // give game dimensions
+  // give game dimensions 640 x 360
   // phaser use WebGL or default to canvas if WebGL is not avail
   // Pixi.js does the rendering of the WebGL
   var game = new Phaser.Game(640, 360, Phaser.AUTO); 
@@ -10,7 +10,7 @@
   var GameState = {
     // all image loaded
     preload: function() {
-      this.load.image('background', 'assets/images/earth_1920_1080.jpg');
+      this.load.image('background', 'assets/images/earth_640x360.jpg');
     },
     create: function() {
       this.background = this.game.add.sprite(0, 0, 'background') // coords top left, key
